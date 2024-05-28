66f06f6ece3ffd30eeaece03036f233769c61e0c
Tim Apitius <timapitius@gmail.com>
2024-05-28 14:21:57 +0200

    add crud actions for ingredients

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,2 +1,30 @@
 class IngredientsController < ApplicationController
+  def index
+    @ingredients = current_user.ingredients
+  end
+
+  def show
+    @ingredient = Ingredient.find(params[:id])
+  end
+
+  def new
+    @ingredient = Ingredient.new
+  end
+
+  def create
+    @ingredient = Ingredient.new(ingredient_params)
+    @ingredient.user = current_user
+
+    if @ingredient.save
+      redirect_to @ingredient
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def ingredient_params
+    params.require(:ingredient).permit(:name, :amount, :measurement)
+  end
 end
