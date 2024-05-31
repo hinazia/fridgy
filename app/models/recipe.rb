8ab097009fb5a93c0c87c69f3dfd533b41b5c4dc
Iordan Pinto <pintoiordan@gmail.com>
2024-05-31 12:57:25 +0200

    recipe page done

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,6 @@
 class Recipe < ApplicationRecord
   belongs_to :user
-  has_many :ingredient_recipe
-  has_many :ingredient, through: :ingredient_recipe
+  has_many :ingredient_recipes
+  has_many :ingredients, through: :ingredient_recipes
   validates :category, :meal_type, presence: true
 end
