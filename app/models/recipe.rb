f232831a1e731bf0754f52aef6cb5dc4abb290c4
hinaqazi <hinaqazi612@gmail.com>
2024-06-03 17:04:06 +0200

    ai image generate

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,7 +1,9 @@
+require "open-uri"
 class Recipe < ApplicationRecord
   belongs_to :user
   has_many :ingredient_recipes
   has_many :ingredients, through: :ingredient_recipes
+  has_one_attached :photo
   validates :category, :meal_type, presence: true
 
   include PgSearch::Model
@@ -10,4 +12,25 @@ class Recipe < ApplicationRecord
      using: {
       tsearch: { prefix: true } # <-- now `superman batm` will return something!
   }
+
+  after_save if: -> { saved_change_to_title? || saved_change_to_content? } do
+    set_photo
+  end
+
+  private
+
+  def set_photo
+    client = OpenAI::Client.new
+    response = client.images.generate(parameters: {
+      prompt: "A recipe image of #{title}", size: "256x256"
+    })
+
+    url = response["data"][0]["url"]
+    file =  URI.open(url)
+
+    photo.purge if photo.attached?
+    photo.attach(io: file, filename: "ai_generated_image.jpg", content_type: "image/png")
+    return photo
+  end
+
 end
